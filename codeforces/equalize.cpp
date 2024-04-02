a36eeccf5f0827f34169857c8f332a38d965f1cc
Sushant <sushantnadavade006@gmail.com>
2024-04-02 22:06:27 +0530

    question added

---
+++ codeforces/equalize.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define ff(n) for(int i=0; i<n; i++)
+#define ff1(n) for(int i=1; i<n; i++)
+#define ll long long
+
+
+
+void solve(){
+    int n;
+    cin>>n;
+
+    vector<int> a(n);
+
+    for(int i=0; i<n; i++)
+       cin>>a[i];
+
+    sort(a.begin(), a.end());
+
+    int i=0, j=n-1;
+
+    while()   
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
