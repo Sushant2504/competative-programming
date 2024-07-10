957f2de4f15868cc837e36b10d4dad652e18ddec
Sushant <sushantnadavade006@gmail.com>
2024-07-10 23:54:32 +0530

    new code is added

---
+++ codeforces/basil's_garden.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define ff(n) for(int i=0; i<n; i++)
+#define ff1(n) for(int i=1; i<n; i++)
+#define ll long long
+
+
+void solve(){
+    
+}
+
+
+int main(){
+     int t;
+     cin>>t;
+     while(t--){
+        solve();
+     }
+}
