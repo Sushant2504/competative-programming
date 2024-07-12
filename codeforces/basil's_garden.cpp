e919fd2f659623540db1fca366d1fb30047ed9fa
Sushant <sushantnadavade006@gmail.com>
2024-07-12 14:34:30 +0530

    new code is added

--- codeforces/basil's_garden.cpp
+++ codeforces/basil's_garden.cpp
@@ -8,7 +8,20 @@ using namespace std;
 
 
 void solve(){
+    int n;
+    cin>>n;
+
+    vector<int> v(n);
+    ff(n) cin>>v[i];
+
+    int ans = v[n-1];
+    for(int i=n-2; i>=0; i--){
+         ans = max(ans+1, v[i]);
+    }
+
     
+    cout<<ans<<endl;
+
 }
 
 
---
+++ codeforces/update_queries.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define ff(n) for (int i = 0; i < n; i++)
+#define ff1(n) for (int i = 1; i < n; i++)
+const int M = 1e9 + 7;
+
+
+void solve() {
+     int m, n;
+     cin>>n>>m;
+
+     
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
